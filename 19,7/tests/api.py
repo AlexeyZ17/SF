e643f9c5d36f62f39fda5783813bad293dec7f3a
Alexey <102945253+AlexeyZ17@users.noreply.github.com>
2022-09-01 18:19:08 +0300

    Add files via upload

---
+++ 19,7/tests/api.py
@@ -0,0 +1,98 @@
+import json
+import requests
+from requests_toolbelt.multipart.encoder import MultipartEncoder
+
+
+class PetFriends:
+
+    def __init__(self):
+        self.base_url = "https://petfriends.skillfactory.ru/"
+
+    def get_api_key(self, email: str, passwd: str) -> json:
+
+        headers = {
+            'email': email,
+            'password': passwd,
+        }
+        res = requests.get(self.base_url+'api/key', headers=headers)
+        status = res.status_code
+        result = ""
+        try:
+            result = res.json()
+        except json.decoder.JSONDecodeError:
+            result = res.text
+        return status, result
+
+
+    # запрос с фильтром мои питомцы
+    def get_list_of_pets(self, auth_key: json, filter: str = "my_pets") -> json:
+
+        headers = {'auth_key': auth_key['key']}
+        filter = {'filter': filter}
+
+        res = requests.get(self.base_url + 'api/pets', headers=headers, params=filter)
+        status = res.status_code
+        result = ""
+        try:
+            result = res.json()
+        except json.decoder.JSONDecodeError:
+            result = res.text
+        return status, result
+
+
+    # Добавление питомца с корректными данными
+    def add_new_pet(self, auth_key: json, name: str, animal_type: str, age: str, pet_photo: str) -> json:
+
+        data = MultipartEncoder(
+            fields={
+                'name': name,
+                'animal_type': animal_type,
+                'age': age,
+                'pet_photo': (pet_photo, open(pet_photo, 'kot'), 'image/jpg')
+            })
+        headers = {'auth_key': auth_key['key'], 'Content-Type': data.content_type}
+
+        res = requests.post(self.base_url + 'api/pets', headers=headers, data=data)
+        status = res.status_code
+        result = ""
+        try:
+            result = res.json()
+        except json.decoder.JSONDecodeError:
+            result = res.text
+        print(result)
+        return status, result
+
+
+    # Добавление фото питомца к уже ранее добавленному животному, через его ID
+    def update_pet_info(self, auth_key: json, pet_id: str, name: str, animal_type: str, age: int) -> json:
+
+        headers = {'auth_key': auth_key['key']}
+        data = {
+            'name': name,
+            'age': age,
+            'animal_type': animal_type
+        }
+
+        res = requests.put(self.base_url + 'api/pets/' + pet_id, headers=headers, data=data)
+        status = res.status_code
+        result = ""
+        try:
+            result = res.json()
+        except json.decoder.JSONDecodeError:
+            result = res.text
+        return status, result
+
+
+# удаление питомца с использованием ID
+    def delete_pet(self, auth_key: json, pet_id: str) -> json:
+
+        headers = {'auth_key': auth_key['key']}
+
+        res = requests.delete(self.base_url + 'api/pets/' + pet_id, headers=headers)
+        status = res.status_code
+        result = ""
+        try:
+            result = res.json()
+        except json.decoder.JSONDecodeError:
+            result = res.text
+        return status, result
---
+++ 19,7/tests/settings.py
@@ -0,0 +1,8 @@
+valid_email = "luckin.alexej@yandex.ru"
+valid_password = "Lav123456"
+# api_key = "key": "ea738148a1f19838e1c5d1413877f3691a3731380e733e877b0ae729"
+
+none_valid_email = "luckin.alexe@yandex.ru"
+none_valid_password = "Lav12345"
+none_valid_key = "12356hdtfdtcj"
+none_api_key = "12"
